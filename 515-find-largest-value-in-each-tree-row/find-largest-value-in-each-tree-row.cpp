acdae47f339b7cc03a19e94e24b830aaff609172
Muhammad Taha Ali <38587607+m-tahaali@users.noreply.github.com>
2023-10-24 20:38:12 +0500

    Time: 8 ms (68.56%) | Memory: 22.5 MB (50.91%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
@@ -10,22 +10,23 @@
  * };
  */
 
-void iterate(vector<int> &rows, TreeNode* root, int row) {
-    if (root) {
-        while (rows.size() < row + 1) rows.push_back(INT_MIN);
-        if (rows[row] < root->val) {
-            rows[row] = root->val;
-        }
-        iterate(rows, root->right, row+1);
-        iterate(rows, root->left, row+1);
-    }
-}
-
 class Solution {
 public:
+    vector<int> rows;
+
+    void iterate(TreeNode* root, int row) {
+        if (root) {
+            while (rows.size() < row + 1) rows.push_back(INT_MIN);
+            if (rows[row] < root->val) {
+                rows[row] = root->val;
+            }
+            iterate(root->right, row+1);
+            iterate(root->left, row+1);
+        }
+    }
+
     vector<int> largestValues(TreeNode* root) {
-        vector<int> rows;
-        iterate(rows, root, 0);
+        iterate(root, 0);
         return rows;
     }
 };
