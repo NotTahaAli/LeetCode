94abb0d596e753ba65864f15282acb313ec70151
Muhammad Taha Ali <38587607+m-tahaali@users.noreply.github.com>
2023-12-19 18:21:19 +0500

    Time: 49 ms (26.77%) | Memory: 25.8 MB (8.31%) - LeetSync

--- 661-image-smoother/image-smoother.cpp
+++ 661-image-smoother/image-smoother.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int calculateCell(vector<vector<int>>& img, int i, int j, int m, int n) {
-        int vals = 0;
-        int total = 0;
-        for (int x = -1; x <= 1; x++) {
-            for (int y = -1; y <= 1; y++) {
-                if (i+x >= m || i + x < 0 || j+y >= n || j+y < 0) continue;
-                vals++;
-                total += img[i+x][j+y];
-            }
-        }
-        total /= vals;
-        return total;
-    }
     vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
         vector<vector<int>> newimg;
         int m = img.size();
         int n = img[0].size();
+        int vals = 0;
+        int total = 0;
         for (int i = 0; i < m; i++) {
             vector<int> row;
             for (int j = 0; j < n; j++) {
-                row.push_back(calculateCell(img,i,j,m,n));
+                vals = 0;
+                total = 0;
+                for (int x = -1; x <= 1; x++) {
+                    for (int y = -1; y <= 1; y++) {
+                        if (i+x >= m || i + x < 0 || j+y >= n || j+y < 0) continue;
+                        vals++;
+                        total += img[i+x][j+y];
+                    }
+                }
+                row.push_back(total / vals);
             }
             newimg.push_back(row);
         }
