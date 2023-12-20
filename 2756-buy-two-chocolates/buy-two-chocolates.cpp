24d1a4c1e72334dea94f2c7289e3198f934fff8c
Muhammad Taha Ali <38587607+m-tahaali@users.noreply.github.com>
2023-12-20 17:16:07 +0500

    Time: 11 ms (73.75%) | Memory: 46.9 MB (58.30%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int min1 = INT_MAX;
+        int min2 = INT_MAX;
+        int n = prices.size();
+        for (int i = 0; i < n; i++) {
+            if (prices[i] < min1) {
+                min2 = min1;
+                min1 = prices[i];
+            } else if (prices[i] < min2) {
+                min2 = prices[i];
+            }
+        }
+        if (min2+min1 <= money) return money-min1-min2;
+        return money;
+    }
+};
