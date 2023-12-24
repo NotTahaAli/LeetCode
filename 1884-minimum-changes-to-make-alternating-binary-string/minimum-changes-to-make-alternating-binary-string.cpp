14733f3c66c3679485559c2d6f7902c094cd1845
Muhammad Taha Ali <38587607+m-tahaali@users.noreply.github.com>
2023-12-25 01:59:11 +0500

    Time: 0 ms (100.00%) | Memory: 7.2 MB (98.50%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,43 +1,16 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int minChanges = INT_MAX;
         int changes = 0;
-        bool lastChanged = false;
-        for (int i = 1; i < s.size(); i++) {
-            if (s[i] == s[i-1]) {
+        int next = 1;
+        int strlen = s.size();
+        for (int i = 0; i < strlen; i++) {
+            if (s[i]-'0' != next) {
                 changes++;
-                if (i > 1 && s[i-1] != s[i-2]) {
-                    changes = INT_MAX;
-                    break;
-                }
             }
+            next = !next;
         }
-        minChanges = min(changes, minChanges);
-        for (int i = 1; i < s.size(); i++) {
-            if (s[i] != s[i-1]) continue;
-            changes = 0;
-            lastChanged = true;
-            for (int j = i-1; changes < minChanges && j >= 0; j--) {
-                if ((!lastChanged && s[j] == s[j+1]) || (lastChanged && s[j] != s[j+1])) {
-                    changes++;
-                    lastChanged = true;
-                } else {
-                    lastChanged = false;
-                }
-            }
-            lastChanged = false;
-            for (int j = i; changes < minChanges && j < s.size(); j++) {
-                if ((!lastChanged && s[j] == s[j-1]) || (lastChanged && s[j] != s[j-1])) {
-                    changes++;
-                    lastChanged = true;
-                } else {
-                    lastChanged = false;
-                }
-            }
-            minChanges = min(changes, minChanges);
-        }
-        minChanges = min(changes, minChanges);
-        return minChanges;
+        changes = min(changes, strlen-changes);
+        return changes;
     }
 };
