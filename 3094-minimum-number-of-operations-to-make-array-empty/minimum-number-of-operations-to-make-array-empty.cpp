8ba58007949ba41ec4cad85ba4b3e9002c400fdf
Muhammad Taha Ali <38587607+m-tahaali@users.noreply.github.com>
2024-01-04 17:13:38 +0500

    Time: 129 ms (36.65%) | Memory: 85.1 MB (90.06%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -7,17 +7,8 @@ public:
             counts[nums[i]]++;
         }
         for (auto i: counts) {
-            while (i.second >= 5) {
-                changes++;
-                i.second -= 3;
-            }
-            if (i.second%3 != 1) {
-                changes += i.second / 3;
-                i.second %= 3;
-            }
-            changes += i.second / 2;
-            i.second %= 2;
-            if (i.second != 0) return -1;
+            if (i.second == 1) return -1;
+            changes += (i.second+2)/3;
         }
         return changes;
     }
