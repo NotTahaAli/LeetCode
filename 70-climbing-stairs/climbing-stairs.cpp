a8d0bd43bce66538c9e4a0098dadc0ec60feff91
Muhammad Taha Ali <38587607+m-tahaali@users.noreply.github.com>
2024-01-18 19:39:22 +0500

    Time: 0 ms (100.00%) | Memory: 6.4 MB (59.54%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int possible = 0;
+        int maxCountOf2 = n/2;
+        int temp;
+        int numberof2;
+        int numberof1;
+        for(int i = 0; i <= maxCountOf2; i++) {
+            numberof2 = i;
+            numberof1 = n-i*2;
+            temp = tgamma(numberof2+numberof1+1) / (tgamma(numberof1+1) * tgamma(numberof2+1)) + 0.5;
+            possible += temp;
+        }
+        return possible;
+    }
+};
