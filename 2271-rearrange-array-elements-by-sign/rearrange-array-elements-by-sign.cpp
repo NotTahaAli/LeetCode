d9c4bbfe070dcbc9aeca54816a0ec555ffb9b17d
Muhammad Taha Ali <38587607+m-tahaali@users.noreply.github.com>
2024-02-14 09:26:32 +0500

    Time: 674 ms (5.00%) | Memory: 132.5 MB (35.06%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> out;
+        int lastPos = -1;
+        int lastNeg = -1;
+        for (int pos = 0; pos < nums.size(); pos++) {
+            for (int j = ((pos % 2 == 0) ? lastPos : lastNeg) + 1; j < nums.size(); j++) {
+                if ((pos % 2 == 0 && nums[j] > 0) || (pos % 2 != 0 && nums[j] < 0)) {
+                    if (nums[j] > 0) {
+                        lastPos = j;
+                    } else {
+                        lastNeg = j;
+                    }
+                    out.push_back(nums[j]);
+                    break;
+                }
+            }
+            cout << lastPos << " " << lastNeg << endl;
+        }
+        return out;
+    }
+};
