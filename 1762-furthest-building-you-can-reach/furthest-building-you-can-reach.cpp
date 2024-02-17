1e980bd45ee0745216dcc3702f8ec1d9ac43bd32
Muhammad Taha Ali <38587607+m-tahaali@users.noreply.github.com>
2024-02-17 11:11:30 +0500

    Time: 454 ms (5.01%) | Memory: 57.9 MB (17.23%) - LeetSync

---
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        vector<int> bricksNeeded(heights.size(),0);
+        int maxDistance = 0;
+        int prevMaxDistance;
+        for (int i = 1; i < heights.size(); i++) {
+            bricksNeeded[i] = max(heights[i]-heights[i-1],0);
+        }
+        do {
+            for (auto b: bricksNeeded) {
+                cout << b << " ";
+            }
+            int tempBricks = bricks;
+            int tempLadders = ladders;
+            cout << endl;
+            prevMaxDistance = maxDistance;
+            int i = 1;
+            for (; i < heights.size(); i++) {
+                if (bricksNeeded[i] > tempBricks) {
+                    if (tempLadders == 0) {
+                        break;
+                    } else {
+                        tempLadders--;
+                    }
+                } else {
+                    tempBricks -= bricksNeeded[i];
+                }
+            }
+            maxDistance = i - 1;
+            sort(bricksNeeded.begin(), bricksNeeded.begin()+i);
+        } while (prevMaxDistance < maxDistance);
+        return max(prevMaxDistance,maxDistance);
+    }
+};
