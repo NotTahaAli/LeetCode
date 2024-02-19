3cbe23fb5d8ca8f279aed2203d0213538ac97391
Muhammad Taha Ali <38587607+m-tahaali@users.noreply.github.com>
2024-02-19 14:45:56 +0500

    Time: 5 ms (20.09%) | Memory: 7.2 MB (39.77%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -5,9 +5,10 @@ public:
             return false;
         int count1s = 0;
         for (int i = 0; i < 31; i++) {
-            if (((n >> i) & 0x1) == 1) {
+            if ((n & 0x1) == 1) {
                 count1s++;
             }
+            n >>= 1;
         }
         return count1s == 1;
     }
