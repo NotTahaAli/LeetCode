acf270bdca1efd4f2726dfb93e50c5039129d0ec
Muhammad Taha Ali <38587607+m-tahaali@users.noreply.github.com>
2024-02-19 14:50:06 +0500

    Time: 0 ms (100.00%) | Memory: 7.2 MB (39.77%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if((n >> 31) != 0)
-            return false;
-        int count1s = 0;
-        for (int i = 0; i < 31; i++) {
-            if ((n & 0x1) == 1) {
-                count1s++;
-            }
-            n >>= 1;
-        }
-        return count1s == 1;
+        return n > 0 && !(n & n-1);
     }
 };
