bd0368ff1b53fa84dac8c1c3cea80489665a471b
Muhammad Taha Ali <38587607+m-tahaali@users.noreply.github.com>
2024-02-22 20:24:55 +0500

    Time: 131 ms (71.51%) | Memory: 72.4 MB (26.18%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int findJudge(const int n, vector<vector<int>>& trust) {
-        vector<pair<bool, int>> trusts(n,{0,0});
+        vector<int> trusts(n,0);
 
         for (auto t: trust) {
-            trusts[t[0]-1].first = true;
-            trusts[t[1]-1].second++;
+            trusts[t[0]-1]--;
+            trusts[t[1]-1]++;
         }
         for (int i = 0; i < n; i++) {
-            if (trusts[i].first == false && trusts[i].second == n-1) return i+1;
+            if (trusts[i] == n-1) return i+1;
         }
         return -1;
     }
