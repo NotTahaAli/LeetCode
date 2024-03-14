269b1cccd3551f34440ad65220b30e9e4aef3561
Muhammad Taha Ali <38587607+NotTahaAli@users.noreply.github.com>
2024-03-15 00:09:55 +0500

    Time: 545 ms (5.02%) | Memory: 31 MB (94.04%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         int count = 0;
-        for (int right = 0, sum = 0; right < nums.size(); right++) {
+        for (int left = 0, right = 0, sum = 0; right < nums.size(); right++) {
             sum += nums[right];
-            int currSum = sum;
-            for (int left = 0, currSum = sum; left <= right; left++) {
+            for (int i = left, currSum = sum; i <= right; i++) {
                 if (currSum == goal) {
                     count++;
                 }
                 if (currSum < goal) break;
-                currSum -= nums[left];
+                currSum -= nums[i];
+            }
+            while (right > left && (sum > goal || right == nums.size() - 1)) {
+                sum -= nums[left];
+                left++;
             }
         }
         return count;
